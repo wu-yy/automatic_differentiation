77bb61c4953e000a05e3c3cf47ba5cd35352ad60
蓝鲸123 <wuyongyu13@126.com>
2020-06-01 15:22:44 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,37 @@
+import autodiff as ad
+import numpy as np
+
+def test_variable():
+    x2 = ad.Variable(name = "x2")
+    y = x2
+
+    grad_x2, = ad.gradients(y, [x2])
+
+    executor = ad.Executor([y, grad_x2])
+    x2_val = 2 * np.ones(3)
+    y_val, grad_x2_val= executor.run(feed_dict = {x2 : x2_val})
+
+    assert isinstance(y, ad.Node)
+    assert np.array_equal(y_val, x2_val)
+    assert np.array_equal(grad_x2_val, np.ones_like(x2_val))
+
+# test_variable()
+
+def test_add_by_const():
+    x2 = ad.Variable(name = "x2")
+    y = 5 + x2
+
+    grad_x2, = ad.gradients(y, [x2])
+
+    # print(grad_x2)
+
+    executor = ad.Executor([y, grad_x2])
+    x2_val = 2 * np.ones(3)
+    y_val, grad_x2_val= executor.run(feed_dict = {x2 : x2_val})
+
+    assert isinstance(y, ad.Node)
+    assert np.array_equal(y_val, x2_val + 5)
+    assert np.array_equal(grad_x2_val, np.ones_like(x2_val))
+
+test_add_by_const()
+
